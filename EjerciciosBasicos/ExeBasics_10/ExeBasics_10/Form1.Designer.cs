bce1ccce4b4ac76fa69f6fbe4ca8321b501031b7
xexe <francesccoll@paucasesnovescifp.cat>
2020-10-16 23:31:30 +0200

    exe 10 scroll bar 50 a 80%

---
+++ EjerciciosBasicos/ExeBasics_10/ExeBasics_10/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace ExeBasics_10
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ScrollBar = new System.Windows.Forms.HScrollBar();
+            this.NumberBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // ScrollBar
+            // 
+            this.ScrollBar.Location = new System.Drawing.Point(28, 69);
+            this.ScrollBar.Maximum = 90;
+            this.ScrollBar.Minimum = 50;
+            this.ScrollBar.Name = "ScrollBar";
+            this.ScrollBar.Size = new System.Drawing.Size(639, 17);
+            this.ScrollBar.TabIndex = 0;
+            this.ScrollBar.Value = 50;
+            this.ScrollBar.Scroll += new System.Windows.Forms.ScrollEventHandler(this.ScrollBar_Scroll);
+            // 
+            // NumberBox
+            // 
+            this.NumberBox.Location = new System.Drawing.Point(99, 348);
+            this.NumberBox.Name = "NumberBox";
+            this.NumberBox.Size = new System.Drawing.Size(469, 23);
+            this.NumberBox.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(714, 450);
+            this.Controls.Add(this.NumberBox);
+            this.Controls.Add(this.ScrollBar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox NumberBox;
+        public System.Windows.Forms.HScrollBar ScrollBar;
+    }
+}
+
---
+++ EjerciciosBasicos/ExeBasics_10/ExeBasics_10/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExeBasics_10
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void ScrollBar_Scroll(object sender, ScrollEventArgs e)
+        {
+            int valor = ScrollBar.Value;
+            NumberBox.Text = valor.ToString() + " %";
+        }
+
+        //Hen los parametros de la scrolbar he puesto que el numero maximo sea 90 porque si ponia 80 en la textbar solo me llegava hasta el numero 70
+    }
+}
